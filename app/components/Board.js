bc6f1bf3bc6b6582b0900ff9721c3883a591a41b
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-06-18 10:23:06 +0200

    step 1

--- app/components/Board.js
+++ app/components/Board.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
-    }
 
     render() {
         const status = 'Next player: X';
@@ -13,19 +10,19 @@ class Board extends React.Component {
               <div>
                   <div className="status">{status}</div>
                   <div className="board-row">
-                      <Square />
-                      <Square />
-                      <Square />
+                      <Square value={0}/>
+                      <Square value={1}/>
+                      <Square value={2}/>
                   </div>
                   <div className="board-row">
-                      <Square />
-                      <Square />
-                      <Square />
-                    </div>
+                      <Square value={3}/>
+                      <Square value={4}/>
+                      <Square value={5}/>
+                  </div>
                   <div className="board-row">
-                      <Square />
-                      <Square />
-                      <Square />
+                      <Square value={6}/>
+                      <Square value={7}/>
+                      <Square value={8} />
                   </div>
               </div>
         );
