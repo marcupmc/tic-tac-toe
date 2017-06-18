65cb1862fee36fdc4dda1ef9c80df338f268331d
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-06-18 10:17:08 +0200

    call square without renderSquare

--- app/components/Board.js
+++ app/components/Board.js
@@ -11,22 +11,22 @@ class Board extends React.Component {
 
         return (
               <div>
-                    <div className="status">{status}</div>
-                    <div className="board-row">
-                          {this.renderSquare(0)}
-                          {this.renderSquare(1)}
-                          {this.renderSquare(2)}
+                  <div className="status">{status}</div>
+                  <div className="board-row">
+                      <Square />
+                      <Square />
+                      <Square />
+                  </div>
+                  <div className="board-row">
+                      <Square />
+                      <Square />
+                      <Square />
                     </div>
-                    <div className="board-row">
-                          {this.renderSquare(3)}
-                          {this.renderSquare(4)}
-                          {this.renderSquare(5)}
-                    </div>
-                    <div className="board-row">
-                          {this.renderSquare(6)}
-                          {this.renderSquare(7)}
-                          {this.renderSquare(8)}
-                    </div>
+                  <div className="board-row">
+                      <Square />
+                      <Square />
+                      <Square />
+                  </div>
               </div>
         );
     }
