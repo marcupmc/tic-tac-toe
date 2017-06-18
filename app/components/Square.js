979ce9f3c7aa06cbf2a3651248662e08b412415d
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-06-18 10:28:46 +0200

    step 2

--- app/components/Square.js
+++ app/components/Square.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-              <button className="square">
-                {this.props.value}
+              <button className="square" onClick={() => this.setState({ value: 'X' })}>
+                  {this.state.value}
               </button>
         );
     }
