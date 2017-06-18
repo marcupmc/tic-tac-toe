0ed414569b59bc3573a690dd69e7176481487c97
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-06-18 10:43:19 +0200

    step 3

--- app/components/Board.js
+++ app/components/Board.js
@@ -3,6 +3,23 @@ import Square from './Square';
 
 class Board extends React.Component {
 
+    constructor(props) {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(numSquare) {
+        const squares = this.state.squares.slice();
+        squares[numSquare] = 'X';
+        this.setState({ squares: squares });
+    }
+
+    renderSquare(i) {
+        return (<Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />);
+    }
+
     render() {
         const status = 'Next player: X';
 
@@ -10,19 +27,19 @@ class Board extends React.Component {
               <div>
                   <div className="status">{status}</div>
                   <div className="board-row">
-                      <Square value={0}/>
-                      <Square value={1}/>
-                      <Square value={2}/>
+                      {this.renderSquare(0)}
+                      {this.renderSquare(1)}
+                      {this.renderSquare(2)}
                   </div>
                   <div className="board-row">
-                      <Square value={3}/>
-                      <Square value={4}/>
-                      <Square value={5}/>
+                      {this.renderSquare(3)}
+                      {this.renderSquare(4)}
+                      {this.renderSquare(5)}
                   </div>
                   <div className="board-row">
-                      <Square value={6}/>
-                      <Square value={7}/>
-                      <Square value={8} />
+                      {this.renderSquare(6)}
+                      {this.renderSquare(7)}
+                      {this.renderSquare(8)}
                   </div>
               </div>
         );
